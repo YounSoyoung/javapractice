33ae8386031f949d34181c2803e072ad58da6948
YounSoyoung <grace902853@gmail.com>
2023-03-21 14:08:50 +0900

    poly

--- src/main/java/poly/Ex03.java
+++ src/main/java/poly/Ex03.java
@@ -13,7 +13,36 @@ class Tire {
     public Tire(){
         tireName = "정품타이어";
     }
+
+    public String getTireName() {
+        return tireName;
+    }
+}
+
+class HankookTire extends Tire {
+    public HankookTire(){
+        tireName = "한국타이어";
+    }
+}
+
+class KumhoTire extends Tire {
+    public KumhoTire() {
+        tireName = "금호타이어";
+    }
 }
 
 public class Ex03 {
+    public static void main(String[] args) {
+        Car car = new Car();
+        String tireName1 = car.tire.getTireName();
+        System.out.println(tireName1);
+
+        car.tire = new HankookTire(); //필드의 다형성
+        String tireName2 = car.tire.getTireName();
+        System.out.println(tireName2);
+
+        car.tire = new KumhoTire();
+        String tireName3 = car.tire.getTireName();
+        System.out.println(tireName3);
+    }
 }
